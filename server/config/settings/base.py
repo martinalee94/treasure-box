65e6b053d7f23e4625b4321fad9cf03d54e47290
Martina Lee <martinalee0528@gmail.com>
2022-04-13 01:39:44 +0900

    chore: mysql 연동

--- server/config/settings/base.py
+++ server/config/settings/base.py
@@ -63,8 +63,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": env("MYSQL_NAME"),
+        "USER": env("MYSQL_USER"),
+        "PASSWORD": env("MYSQL_PASSWORD"),
+        "HOST": env("MYSQL_HOST"),
+        "PORT": env("MYSQL_PORT"),
     }
 }
 
