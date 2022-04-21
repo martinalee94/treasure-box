d78f7037b468011014a0cc85286cbbda1f95df2a
Martina Lee <martinalee0528@gmail.com>
2022-04-21 10:25:44 +0900

    feat: heroku 환경변수 분리

--- server/config/settings/base.py
+++ server/config/settings/base.py
@@ -6,7 +6,7 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 env = environ.Env(DEBUG=(bool, True))
-environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
+# environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
