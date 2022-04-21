ffc3fffff5fa919b7f94bc214c7b95030bbfe218
Martina Lee <martinalee0528@gmail.com>
2022-04-21 09:47:28 +0900

    feat: 구글 로그인

--- server/apps/users/urls.py
+++ server/apps/users/urls.py
@@ -1,3 +1,8 @@
 from django.urls import include, path
 
-urlpatterns = []
+# from .views import GoogleLogin, SelfGoogleLogin
+
+urlpatterns = [
+    # path("dj-rest-auth/google/", GoogleLogin.as_view(), name="google_login"),
+    # path("auth/google", SelfGoogleLogin.as_view()),
+]
