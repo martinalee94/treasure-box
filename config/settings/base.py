c9e0087e0cded1f114f773335a61d3d21004f531
Martina Lee <martinalee0528@gmail.com>
2022-04-22 22:12:10 +0900

    chore: 프로젝트 구조 변경

--- config/settings/base.py
+++ config/settings/base.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(DEBUG=(bool, True))
 # environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
 
