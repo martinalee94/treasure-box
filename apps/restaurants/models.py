0d4e862151ff8f21fa641121f2a256140472b13d
Martina Lee <martinalee0528@gmail.com>
2022-05-13 23:34:02 +0900

    feat: 가게 모델 베이스 추가

--- apps/restaurants/models.py
+++ apps/restaurants/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=50)
+    street_addr = models.CharField(max_length=256)
+    bunji_addr = models.CharField(max_length=256)
+    avg_rating = models.DecimalField(max_digits=3, decimal_places=2)
+    lat = models.FloatField()
+    lng = models.FloatField()
+    operation_hr = models.CharField(max_length=512)
+    main_img = models.URLField()
